ea6f7411bf3eec13b131bed4283fce4f850a7e0d
Arnaud Morin <arnaud1.morin@orange.com>
2015-01-20 16:53:26 +0100

    Add python-openstackclient from pip

--- manifests/keystone.pp
+++ manifests/keystone.pp
@@ -13,32 +13,46 @@
 #  The profile to install keystone
 #
 class opensteak::keystone {
+  include pip
+  
   $password = hiera('mysql::service-password')
   $domain = hiera('domain')
   $stack_domain = hiera('stack::domain')
   
   # Probleme: c'est dans la machine mysql qu'on créer la BDD
   # le script keystone-manage y est appelé mais n'existe pas
-  # On l'appelle donc ici
-  # A mon avis, y'a un bug à ouvrir...
-  exec { '/bin/sh -c "keystone-manage db_sync" keystone': }
+  # Decommenter au besoin pour l'appeler
+  # Il est normalement appeler à l'install du packet keystone
+  # ou quand on modifie le parametre database_connection
+  #class { '::keystone::db::sync': }
 
-  class { 'keystone':
+  # Keystone need an uptodate python-openstackclient, install dependencies
+  package { ['libffi-dev','python-dev']:
+    ensure  => present,
+  }
+  
+  pip::install { 'python-openstackclient':
+    ensure  => present,
+  }
+
+  class { '::keystone':
     verbose                 => hiera('verbose'),
     debug                   => hiera('debug'),
     admin_token             => hiera('keystone::admin-token'),
     database_connection     => "mysql://keystone:${password}@mysql.${stack_domain}/keystone",
+    rabbit_host             => "rabbitmq.${stack_domain}",
+    rabbit_password         => hiera('rabbitmq::password'),
 #    paste_config            => "/etc/keystone/keystone-paste.ini",
 #    token_driver            => "keystone.token.backends.sql.Token",
   }
 
-  class { 'keystone::roles::admin':
+  class { '::keystone::roles::admin':
     email        => hiera('admin::mail'),
     password     => hiera('admin::password'),
     admin_tenant => hiera('admin::tenant'),
   }
 
-  class { 'keystone::endpoint':
+  class { '::keystone::endpoint':
     public_url       => "http://keystone.${stack_domain}:5000",
     admin_url        => "http://keystone.${stack_domain}:35357",
     region           => hiera('region'),
@@ -55,7 +69,7 @@ class opensteak::keystone {
 #    enabled  => True,
 #  }
 
-  class { 'glance::keystone::auth':
+  class { '::glance::keystone::auth':
     password         => hiera('glance::password'),
     public_address   => "glance.${stack_domain}",
     admin_address    => "glance.${stack_domain}",
@@ -63,7 +77,7 @@ class opensteak::keystone {
     region           => hiera('region'),
   }
 
-  class { 'nova::keystone::auth':
+  class { '::nova::keystone::auth':
     password         => hiera('nova::password'),
     public_address   => "nova.${stack_domain}",
     admin_address    => "nova.${stack_domain}",
@@ -71,7 +85,7 @@ class opensteak::keystone {
     region           => hiera('region'),
   }
 
-  class { 'neutron::keystone::auth':
+  class { '::neutron::keystone::auth':
     password         => hiera('neutron::password'),
     public_address   => "neutron.${stack_domain}",
     admin_address    => "neutron.${stack_domain}",
@@ -79,7 +93,7 @@ class opensteak::keystone {
     region           => hiera('region'),
   }
 
-#  class { 'cinder::keystone::auth':
+#  class { '::cinder::keystone::auth':
 #    password         => hiera('cinder::password'),
 #    public_address   => "cinder.${stack_domain}",
 #    admin_address    => "cinder.${stack_domain}",
