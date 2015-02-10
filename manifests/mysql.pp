ec57d28f2602e52109084d4fa6e2fb8d9f864167
Arnaud Morin <arnaud1.morin@orange.com>
2015-02-10 16:42:45 +0100

    Try to handle UTF8 strange behavior

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -58,13 +58,14 @@ class opensteak::mysql {
     password      => hiera('mysql::service-password'),
     allowed_hosts => '%',
   }
-
+  
   # # Cinder
   # class { '::cinder::db::mysql':
   #   password      => hiera('mysql::service-password'),
   #   allowed_hosts => '%',
   #   mysql_module  => '2.2',
   # }
+
 
   # # Heat
   # class { 'heat::db::mysql':
