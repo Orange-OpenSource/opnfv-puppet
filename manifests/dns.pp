e001e1dcc94f02c1a2cd82f433ef0b9f037068e1
David Blaisonneau <david.blaisonneau@orange.com>
2015-02-27 16:24:05 +0100

    Refactoring of DNS for HA nodes

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -14,76 +14,134 @@
 #
 class opensteak::dns {
   include bind
+  
+  ##
+  # VARS
+  ##
 
-  $stack_domain = hiera('stack::domain')
-  $infra_reverse = hiera('infra::reverse_zone')
-  $infra_vm = {
-    'puppet' => hiera('infra::puppet'),
-    'ceph-admin' => hiera('infra::ceph-admin'),
+    # Domains name
+    $stack_domain = hiera('stack::domain')
+    $infra_reverse = hiera('infra::reverse_zone')
+
+    # HA mode
+    $ha_enabled = hiera('stack::ha::enabled'),
+   
+    # Infrastructure tools
+    $infra_vm = {
+      'puppet' => hiera('infra::puppet'),
+      'ceph-admin' => hiera('infra::ceph-admin'),
+      }
+    $infra_vm_names = keys($infra_vm)
+
+    # OpenStack infrastructure
+    $stack_vm = hiera('stack::vm')
+    $stack_vm_names = keys($stack_vm)
+    
+    # Physical nodes
+    $infra_nodes = hiera('infra::nodes')
+    $infra_nodes_names = keys($infra_nodes)
+
+    # TOP DNS
+    $forwarders = hiera('dns::external')
+
+  ##
+  # Options
+  ##
+    file { '/etc/bind/named.conf.options': 
+      content => template('opensteak/named.conf.options.erb'),
     }
-  $infra_vm_names = keys($infra_vm)
-  $stack_vm = hiera('stack::vm')
-  $stack_vm_names = keys($stack_vm)
-  $infra_nodes = hiera('infra::nodes')
-  $infra_nodes_names = keys($infra_nodes)
-  $forwarders = hiera('dns::external')
+    
+  ##
+  # Zones
+  ##
 
-  file { '/etc/bind/named.conf.options': 
-    content => template('opensteak/named.conf.options.erb'),
-  }
+    # Stack subdomain zone file
+    bind::zone { $stack_domain:
+      zone_contact => hiera('dns::contact'),
+      zone_ns      => ["dns.$stack_domain"],
+      zone_serial  => '2014100201',
+      zone_ttl     => '3800',
+      zone_origin  => $stack_domain,
+    }
 
-  # Stack subdomain zone file
-  bind::zone { $stack_domain:
-    zone_contact => hiera('dns::contact'),
-    zone_ns      => ["dns.$stack_domain"],
-    zone_serial  => '2014100201',
-    zone_ttl     => '3800',
-    zone_origin  => $stack_domain,
-  }
+    # reverse
+    bind::zone { $infra_reverse:
+      zone_contact => hiera('dns::contact'),
+      zone_ns      => ["dns.$stack_domain"],
+      zone_serial  => '2014100201',
+      zone_ttl     => '3800',
+      zone_origin  => $stack_domain,
+    }
 
-  # reverse
-  bind::zone { $infra_reverse:
-    zone_contact => hiera('dns::contact'),
-    zone_ns      => ["dns.$stack_domain"],
-    zone_serial  => '2014100201',
-    zone_ttl     => '3800',
-    zone_origin  => $stack_domain,
-  }
+  ##
+  # Records
+  ##
 
-  # Apply to all A records
-  Bind::A {
-    ensure    => 'present',
-    zone_arpa => $infra_reverse,
-    ptr       => true,
-  }
+    # Apply to all A records
+    Bind::A {
+      ensure    => 'present',
+      zone_arpa => $infra_reverse,
+      ptr       => true,
+    }
 
-  # Create all records for stack
-  create_a_record { $stack_vm_names:
-    domain     => $stack_domain,
-    vm_ip_hash => $stack_vm,
-  }
+    # Create all records for stack
+    create_a_record { $stack_vm_names:
+      domain     => $stack_domain,
+      vm_ip_hash => $stack_vm,
+    }
 
-  # Create all records for infra
-  create_a_record { $infra_vm_names:
-    domain     => $stack_domain,
-    vm_ip_hash => $infra_vm,
-  }
+    # Create all records for infra
+    create_a_record { $infra_vm_names:
+      domain     => $stack_domain,
+      vm_ip_hash => $infra_vm,
+    }
 
-  # Create all records for nodes
-  create_a_record { $infra_nodes_names:
-    domain     => $stack_domain,
-    vm_ip_hash => $infra_nodes,
-  }
+    # Create all records for nodes
+    create_a_record { $infra_nodes_names:
+      domain     => $stack_domain,
+      vm_ip_hash => $infra_nodes,
+    }
 
-  # CNAME puppet.stack vers puppet
-  bind::a { "dns.${stack_domain}.":
+    # DNS record
+    bind::a { "dns.${stack_domain}.":
       zone      => $stack_domain,
       hash_data => {"dns" => { owner => hiera('infra::dns'), }, },
-  }
-  
-  #
+    }
+   
+  ##
+  # HA Management
+  ##
+
+    $nodes_cname = ['mysql','keystone','glance','glance-storage','nova','neutron']
+    if str2bool("$ha_enabled" ){fablab-lannion.org
+      # HA VIP record
+      bind::a { "ha.${stack_domain}.":
+          zone      => $stack_domain,
+          hash_data => {"dns" => { owner => hiera('stack::ha::vip'), }, },
+      }
+      # Link nodes to ha VIP
+      bind::record {"${nodes_cname}":
+        zone => $stack_domain,
+        record_type => 'CNAME',
+        hash_data => {
+          $name => { owner => "ha.${stack_domain}.", },
+        }
+      }
+    }
+    else{
+      # Link nodes to nodes1
+      bind::record {"${nodes_cname}":
+        zone => $stack_domain,
+        record_type => 'CNAME',
+        hash_data => {
+          $name => { owner => "${name}1.${stack_domain}.", },
+        }
+      }
+    }
+ 
+  ##
   # Create record type A in bind
-  #
+  ##
   define create_a_record( $vm_name = $title, $domain, $vm_ip_hash){
     bind::a { $vm_name:
       zone      => $domain,
