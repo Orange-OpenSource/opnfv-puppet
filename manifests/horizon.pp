40cb27e3d279926db113e9b2c5d982ee05dfebf0
David Blaisonneau <david.blaisonneau@orange.com>
2015-03-03 17:33:07 +0100

    add horizon declaration

---
+++ manifests/horizon.pp
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2014 Orange Labs
+# 
+# This software is distributed under the terms and conditions of the 'Apache-2.0'
+# license which can be found in the file 'LICENSE.txt' in this package distribution 
+# or at 'http://www.apache.org/licenses/LICENSE-2.0'. 
+#
+# Authors: Arnaud Morin <arnaud1.morin@orange.com> 
+#          David Blaisonneau <david.blaisonneau@orange.com>
+#
+
+#
+#  The profile to install horizon
+#
+class opensteak::horizon {
+
+    $stack_domain = hiera('stack::domain')
+  
+    class { 'memcached':
+        listen_ip => '127.0.0.1',
+        tcp_port  => '11211',
+        udp_port  => '11211',
+    }
+    class { '::horizon':
+        servername            => hiera('horizon::fqdn'),
+        keystone_url          => "http://keystone.${stack_domain}:5000/v2.0",
+        cache_server_ip       => '127.0.0.1',
+        cache_server_port     => '11211',
+        secret_key            => hiera('horizon::secret_key'),
+        django_debug          => hiera('debug'),
+        api_result_limit      => '2000',
+        listen_ssl            => true,
+        horizon_cert          => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+        horizon_ca            => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+        horizon_key           => '/etc/ssl/private/ssl-cert-snakeoil.key',
+    }
+    if $::selinux and str2bool($::selinux) != false {
+        selboolean{'httpd_can_network_connect':
+            value      => on,
+            persistent => true,
+        }
+    }
+}
