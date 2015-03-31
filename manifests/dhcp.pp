3191a1e27100b128a9bd8b5799fac80dce4cbfd2
David Blaisonneau <david.blaisonneau@orange.com>
2015-03-31 10:15:59 +0200

    add dhcp class for foreman installer

---
+++ manifests/dhcp.pp
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2014 Orange Labs
+# 
+# This software is distributed under the terms and conditions of the 'Apache-2.0'
+# license which can be found in the file 'LICENSE.txt' in this package distribution 
+# or at 'http://www.apache.org/licenses/LICENSE-2.0'. 
+#
+# Authors: Arnaud Morin <arnaud1.morin@orange.com> 
+#          David Blaisonneau <david.blaisonneau@orange.com>
+#
+
+#
+# The dhcp class for foreman installer
+#
+
+include stdlib
+
+#~ Variable structure:
+#~      dnsdomain => ['infra.opensteak.fr','0.168.192.in-addr.arpa','1.168.192.in-addr.arpa','2.168.192.in-addr.arpa']
+#~      interfaces => ['eth0','eth1','eth2']
+#~      pools (YAML vars from foreman):
+#~        pools:
+#~          adm.infra.opensteak.fr:
+#~             network: 192.168.1.0
+#~             netmask: 255.255.255.0
+#~             range: 192.168.1.50 192.168.1.99
+#~             gateway: 192.168.1.1
+
+class opensteak::dhcp (
+    $dnsdomain = [$domain],
+    $pools,
+    $nameservers = [$ipaddress],
+    $interfaces = $interfaces,
+    $pxeserver = $ipaddress,
+    $pxefilename  = 'pxelinux.0',
+){
+    class { '::dhcp':
+        dnsdomain    => $dnsdomain,
+        nameservers  => $nameservers,
+        interfaces   => $interfaces,
+        dnsupdatekey => "/etc/bind/rndc.key",
+        pxeserver    => $pxeserver,
+        pxefilename  => $pxefilename,
+    }
+    $poolslist = keys($pools['pools'])
+    define mypool($poolname = $title){
+        $pooldef = $opensteak::dhcp::pools['pools'][$poolname]
+        dhcp::pool{ "pool_${poolname}":
+            network => $pooldef['network'],
+            mask    => $pooldef['netmask'],
+            range   => $pooldef['range'],
+            gateway => $pooldef['gateway'],
+        }
+    }   
+    mypool{$poolslist:}
+}
