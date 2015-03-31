7fe102228300f9143881456ef704d39c85a0b563
arnaudmorin <arnaud.morin@gmail.com>
2015-03-31 14:10:05 +0200

    Update base-network config to reboot the system when finally configured

--- manifests/base-network.pp
+++ manifests/base-network.pp
@@ -4,15 +4,19 @@ class opensteak::base-network (
     package { 'openvswitch-switch':
         ensure => present,
     }
-    
-    # Create main interface file config
-    file { '/etc/network/interfaces':
-        source => "puppet:///modules/opensteak/interfaces",
-    }
 
     # Create bridges and add each interface in it
-    create_bridge_with_interface { $ovs_config: 
+    create_bridge_with_interface { $ovs_config:
         require => Package['openvswitch-switch'],
+        before  => File['/etc/network/interfaces'],
+    }
+
+    # Create main interface file config
+    file { '/etc/network/interfaces':
+        source   => "puppet:///modules/opensteak/interfaces",
+    }
+    reboot { 'after':
+        subscribe => File['/etc/network/interfaces'],
     }
 }
 
@@ -25,31 +29,18 @@ define create_bridge_with_interface {
         ensure => present,
     }
     ~>
-    vs_port {$interface: 
+    vs_port {$interface:
         ensure => present,
         bridge => $bridge,
     }
 
-    # down interface
-    # create interface config file
-    # up interface
-    # create bridge config file
-    # down & up bridge
-    # remove ip on interface if necessary 
-    exec { "/sbin/ifdown ${interface}": }
-    ->
+
     file { "/etc/network/interfaces.d/${interface}.cfg":
         content => template("opensteak/interface.cfg.erb"),
         require => Vs_port[$interface],
     }
-    ->
-    exec { "/sbin/ifup ${interface}":}
-    ->
+
     file { "/etc/network/interfaces.d/${bridge}.cfg":
         content => template("opensteak/bridge.cfg.erb"),
     }
-    ->
-    exec { "/sbin/ifdown ${bridge} && /sbin/ifup ${bridge}":}
-    ->
-    exec { "/sbin/ip a d \$(/sbin/ip addr show ${interface} |/bin/grep ' inet ' | /usr/bin/awk '{print \$2}') dev ${interface}  || /bin/echo ''": }
 }
