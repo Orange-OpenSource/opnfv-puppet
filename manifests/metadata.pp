8edb280dd2b856bc7fe69ac4125694b228594e6a
arnaudmorinol <arnaud1.morin@orange.com>
2015-07-27 18:11:24 +0200

    Add metadata server

---
+++ manifests/metadata.pp
@@ -0,0 +1,74 @@
+#
+# Copyright (C) 2015 Orange Labs
+# 
+# This software is distributed under the terms and conditions of the 'Apache-2.0'
+# license which can be found in the file 'LICENSE.txt' in this package distribution 
+# or at 'http://www.apache.org/licenses/LICENSE-2.0'. 
+#
+# Authors: Arnaud Morin <arnaud1.morin@orange.com> 
+#          David Blaisonneau <david.blaisonneau@orange.com>
+#
+
+#
+#  The profile to install the metadata server
+#
+class opensteak::metadata (
+    $template_folder = '/opt/metadata/templates',
+    $foreman_fqdn = 'foreman.infra.opensteak.fr',
+    $foreman_admin = 'admin',
+    $foreman_password = 'password',
+    $foreman_ip = '127.0.0.1',
+    $metadata_ip = '169.254.169.254/32',
+    $metadata_interface = 'eth0',
+    ){
+    include pip
+
+    # Install prerequisities
+    package { 'python3-tornado':
+        ensure => installed,
+    }
+
+    pip::install { ['beautifulsoup4', 'PyYAML', 'requests', 'requests-futures']:
+        ensure          => present,
+        python_version  => '3.4',
+    }
+    ->
+    # Create binary
+    file { "/usr/local/bin/opensteak-metadata-server.py":
+        content => template("opensteak/opensteak-metadata-server.py.erb"),
+        mode    => "0755",
+    }
+    ->
+    # Create init
+    file { "/etc/init/metadata.conf":
+        ensure  => present,
+        source  => "puppet:///modules/opensteak/metadata_init",
+    }
+    ->
+    # Start Service
+    service { "metadata":
+        ensure  => running,
+        require => Package['python3-tornado'],
+    }
+
+    # Create template folder
+    file { "/opt/metadata":
+        ensure  => "directory",
+        recurse => True,
+        source  => "puppet:///modules/opensteak/metadata/",
+    }
+
+    # Set metadata_ip to the network interface
+    exec { "/sbin/ip a a $metadata_ip dev $metadata_interface": 
+        unless => "/sbin/ip a l $metadata_interface | /bin/grep $metadata_ip",
+    }
+    
+    # Set configuration in foreman apache conf
+    file { "/etc/apache2/sites-enabled/05-foreman.conf":
+        content => template("opensteak/05-foreman.conf.erb"),
+    }
+    
+    # TODO
+    #a2enmod proxy_http
+    #service apache2 restart
+}
